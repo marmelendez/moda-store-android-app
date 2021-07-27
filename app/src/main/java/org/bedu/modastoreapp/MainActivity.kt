2d896e656a86d8925a9baa9ce95d0d978fb83523
Maribel Melendez <lizbethmelendez@live.com>
2021-07-27 16:16:36 -0500

    Create configuration data account activity

--- app/src/main/java/org/bedu/modastoreapp/MainActivity.kt
+++ app/src/main/java/org/bedu/modastoreapp/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_configuration)
+        setContentView(R.layout.activity_config_account)
     }
 }
