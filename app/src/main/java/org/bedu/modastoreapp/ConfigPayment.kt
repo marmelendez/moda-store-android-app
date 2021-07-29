45e7f9d925cc0cfc18dadbf80949de7e44a3e364
Maribel Melendez <lizbethmelendez@live.com>
2021-07-29 17:42:23 -0500

    Create configuration payment and address activities

---
+++ app/src/main/java/org/bedu/modastoreapp/ConfigPayment.kt
@@ -0,0 +1,11 @@
+package org.bedu.modastoreapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfigPayment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_config_payment)
+    }
+}
