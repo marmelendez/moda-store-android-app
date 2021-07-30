3cdb5f647899b1a02c8a2a9dd97b1b221b0a918b
Maribel Melendez <lizbethmelendez@live.com>
2021-07-30 00:41:09 -0500

    Solve bug

--- app/src/main/java/org/bedu/modastoreapp/ConfigurationActivity.kt
+++ app/src/main/java/org/bedu/modastoreapp/ConfigurationActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 
 class ConfigurationActivity : AppCompatActivity() {
+    private lateinit var btn_return: Button
     private lateinit var btn_Account: Button
     private lateinit var btn_Payment: Button
     private lateinit var btn_Address: Button
@@ -15,11 +16,16 @@ class ConfigurationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuration)
 
+        btn_return = findViewById(R.id.btn_Return)
         btn_Account = findViewById(R.id.btn_Account)
         btn_Payment = findViewById(R.id.btn_PaymentMethod)
         btn_Address = findViewById(R.id.btn_Addresses)
         //btn4 = findViewById(R.id.btn_LogOut)
 
+        btn_return.setOnClickListener {
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+        }
 
         btn_Account.setOnClickListener{
             val intent = Intent(this, ConfigAccountDataActivity::class.java)
---
+++ app/src/main/java/org/bedu/modastoreapp/LogInActivity.kt
@@ -0,0 +1,32 @@
+package org.bedu.modastoreapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class LogInActivity : AppCompatActivity() {
+    private lateinit var boton: Button
+    private lateinit var input: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+        boton = findViewById(R.id.buttonLogin)
+        input = findViewById(R.id.textName)
+
+        boton.setOnClickListener {
+            val intent = Intent(this, ConfigurationActivity::class.java)
+            startActivity(intent)
+            /*val boxName = Bundle()
+            boxName.putString(USER_NAME, input.text.toString())
+
+            val intent = Intent(this, ConfigurationActivity::class.java).apply {
+                putExtras(boxName)
+            }
+            startActivity(intent)*/
+        }
+    }
+}
