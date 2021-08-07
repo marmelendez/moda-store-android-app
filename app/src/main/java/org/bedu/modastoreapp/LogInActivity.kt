4d7780f09e6923111488aaa071ff1ccf8402b28a
Tomas <tomas.guerrero.aerospace@gmail.com>
2021-08-06 20:16:59 -0500

    Actualización del carrete

--- app/src/main/java/org/bedu/modastoreapp/LogInActivity.kt
+++ app/src/main/java/org/bedu/modastoreapp/LogInActivity.kt
@@ -7,17 +7,19 @@ import android.widget.Button
 import android.widget.EditText
 
 class LogInActivity : AppCompatActivity() {
-    private lateinit var boton: Button
+    private lateinit var boton_log: Button
+    private lateinit var boton_sign: Button
     private lateinit var input: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
 
-        boton = findViewById(R.id.buttonLogin)
+        boton_log = findViewById(R.id.buttonLogin)
+        boton_sign = findViewById(R.id.buttonSign)
         input = findViewById(R.id.textName)
 
-        boton.setOnClickListener {
+        boton_log.setOnClickListener {
             val intent = Intent(this, ConfigurationActivity::class.java)
             startActivity(intent)
             /*val boxName = Bundle()
@@ -28,5 +30,12 @@ class LogInActivity : AppCompatActivity() {
             }
             startActivity(intent)*/
         }
+
+        boton_sign.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
